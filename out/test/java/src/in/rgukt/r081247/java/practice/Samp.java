0f8dbaaaf551bec523c819426266510db776b098
Vinod Parlapalli <parlapallivinod@gmail.com>
2020-03-31 10:43:26 +0530

    2020-03-31

--- out/test/java/src/in/rgukt/r081247/java/practice/Samp.java
+++ out/test/java/src/in/rgukt/r081247/java/practice/Samp.java
@@ -1,35 +1,29 @@
 package in.rgukt.r081247.java.practice;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Samp {
 
-    private static class CounterThread implements Runnable {
-        private int from;
-        private int count;
-
-        public CounterThread(int from, int count) {
-            this.from = from;
-            this.count = count;
-        }
-        @Override
-        public void run() {
-            for (int i = from; i < from + count; i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println(Thread.currentThread().getName() + ": " + i);
-            }
-        }
+    public static <T, U extends Comparable<? super U>> Comparator<T> comparing(Function<? super T, ? extends U> keyExtractor) {
+        return (key1, key2) -> keyExtractor.apply(key1).compareTo(keyExtractor.apply(key2));
     }
 
-    public static void main(String[] args) throws Exception {
-            new Thread(System.out::println).start();
+    public static <T, U extends Comparable<? super U>> Comparator<T> comparing(Function<? super T, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return (key1, key2) -> keyComparator.compare(keyExtractor.apply(key1), keyExtractor.apply(key2));
+    }
 
 
+    public static void main(String[] args) {
+
+        Stream<String> strings = Stream.of("Z12", "Y1", "X");
+        System.out.println(strings.sorted(comparing(String::length, (key1, key2) -> -(key1 - key2))).collect(Collectors.toList()));
     }
 
 }
