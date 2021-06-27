dd51ce057f43686675b046d968e5698e13a011e5
vinod <vinod@r081247.rgukt.in>
2021-06-27 15:11:36 +0530

    2021/06/27

--- src/in/rgukt/r081247/java/util/practice/Main.java
+++ src/in/rgukt/r081247/java/util/practice/Main.java
@@ -1,56 +1,12 @@
 package in.rgukt.r081247.java.util.practice;
 
-import java.io.IOException;
-import java.sql.Time;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.IntConsumer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        /*
-        String state = switch (State.WAITING) {
-            case CREATED -> {
-                System.out.println("in Created");
-                yield "Created";
-            }
-            case RUNNING -> {
-                System.out.println("in Running");
-                yield "Running";
-            }
-            case WAITING -> {
-                System.out.println("in Waiting");
-                yield "Waiting";
-            }
-            case COMPLETED -> {
-                System.out.println("in Completed");
-                yield "Completed";
-            }
-            default -> {
-                System.out.println("in Default");
-                yield "Default";
-            }
-        };
-
-        System.out.println(state);
-         */
-
-        String json = """
-                {
-                  "id": 2,
-                  "name": "Vinod Parlapalli"
-                }
-                """;
-        System.out.println(json);
+    public static void main(String[] args) {
+        
     }
-
-    public static enum State {
-        CREATED, RUNNING, WAITING, COMPLETED
-    }
-
-
-
 }
