edc077ec7db3131db98283cb562181d2a6bb539d
vinod <vinod@r081247.rgukt.in>
2023-03-16 08:25:05 +0530

    20230316

--- src/in/rgukt/r081247/java/Solution.java
+++ src/in/rgukt/r081247/java/Solution.java
@@ -1,52 +1,27 @@
 package in.rgukt.r081247.java;
 
+import java.io.IOException;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        int overlaps = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            if (checkOverlap(intervals[i], newInterval))
-                overlaps++;
-        }
+    public static void main(String[] args) {
+        Set<String> s = new HashSet<>();
+        s.add(null);
+        s.add(null);
+        System.out.println(s);
+        s.remove(null);
+        s.remove(null);
+        System.out.println(s);
+        Lock lock = new ReentrantLock();
+        Condition c = lock.newCondition();
+        c.signalAll();
 
-        int[][] newIntervals = new int[intervals.length + 1 - overlaps][2];
-        int newIntervalIndex = 0;
-        boolean newIntervalDone = false;
-        for (int i = 0; i < intervals.length; i++) {
-            if (checkOverlap(intervals[i], newInterval)) {
-                newInterval = mergeIntervals(intervals[i], newInterval);
-                continue;
-            } else if (newInterval[0] < intervals[i][0] && (!newIntervalDone)) {
-                newIntervals[newIntervalIndex][0] = newInterval[0];
-                newIntervals[newIntervalIndex][1] = newInterval[1];
-                newIntervalIndex++;
-                newIntervalDone = true;
-            }
-            newIntervals[newIntervalIndex][0] = intervals[i][0];
-            newIntervals[newIntervalIndex][1] = intervals[i][1];
-            newIntervalIndex++;
 
-        }
-
-        if (!newIntervalDone) {
-            newIntervals[newIntervalIndex][0] = newInterval[0];
-            newIntervals[newIntervalIndex][1] = newInterval[1];
-            newIntervalIndex++;
-            newIntervalDone = true;
-        }
-
-        return newIntervals;
-    }
-
-    public boolean checkOverlap(int[] source, int[] target){
-        if (target[0] <= source[1] && target[1] >= source[0])
-            return true;
-        else
-            return false;
-    }
-
-    public int[] mergeIntervals( int[] source, int[] target){
-        if (!checkOverlap(source, target))
-            throw new IllegalArgumentException("intervals don't overlap");
-        return new int[]{source[0] <= target[0] ? source[0] : target[0], source[1] >= target[1] ? source[1] : target[1]};
     }
 }
--- src/in/rgukt/r081247/java/version/java8/Java8Streams.java
+++ src/in/rgukt/r081247/java/version/java8/Java8Streams.java
@@ -1,20 +1,9 @@
-import in.rgukt.r081247.java.datastructure.list.Dequeue;
-import in.rgukt.r081247.java.designpattern.creational.builder.*;
-import in.rgukt.r081247.java.designpattern.creational.factory.Button;
-import in.rgukt.r081247.java.designpattern.structural.bridge.Device;
-import in.rgukt.r081247.java.interviewprep.codingdsalgooops.heap.MergeKSortedLists;
+package in.rgukt.r081247.java.version.java8;
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-
-
-public class Main {
+public class Java8Streams {
     public static class Employee {
         public Long id;
         public String name;
@@ -108,5 +97,3 @@ public class Main {
 
     }
 }
-
-
