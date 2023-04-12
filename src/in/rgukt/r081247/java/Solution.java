b2977ffc6c2e24d7aea836907c8f8e9cc00f4b51
vinod <vinod@r081247.rgukt.in>
2023-04-12 07:42:14 +0530

    20230412

--- src/in/rgukt/r081247/java/Solution.java
+++ src/in/rgukt/r081247/java/Solution.java
@@ -1,26 +1,16 @@
 package in.rgukt.r081247.java;
 
 import java.io.IOException;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class Solution {
     public static void main(String[] args) {
-        Set<String> s = new HashSet<>();
-        s.add(null);
-        s.add(null);
-        System.out.println(s);
-        s.remove(null);
-        s.remove(null);
-        System.out.println(s);
-        Lock lock = new ReentrantLock();
-        Condition c = lock.newCondition();
-        c.signalAll();
+        Optional<Integer> opt = Optional.ofNullable(null);
+        if(opt.isEmpty())
+            System.out.println("empty");
 
 
     }
--- src/in/rgukt/r081247/java/interviewprep/codingdsalgooops/dynamicprogramming/HouseRobber.java
+++ src/in/rgukt/r081247/java/interviewprep/codingdsalgooops/dynamicprogramming/HouseRobber.java
@@ -7,9 +7,14 @@ import java.util.Queue;
 public class HouseRobber {
     public static void main(String[] args) {
         int[] nums = {5, 1, 5, 1, 2, 6, 2, 6};
-        System.out.println(Solution1.rob(nums));
+        System.out.println(Solution3.rob(nums));
     }
 
+
+    /**
+     * Approach   : Dynamic Programming
+     * Complexity : Time: O(n) ; Space: O(2n)
+     */
     public static class Solution1 {
         public static int rob(int[] nums) {
             int[] without = new int[nums.length];
@@ -35,6 +40,10 @@ public class HouseRobber {
         }
     }
 
+    /**
+     * Approach   : Dynamic Programming
+     * Complexity : Time: O(n) ; Space: O(n)
+     */
     public static class Solution2 {
         public static int rob(int[] nums) {
             if(nums.length == 1)
@@ -46,6 +55,52 @@ public class HouseRobber {
                 max[i] = Math.max(max[i-1], max[i-2] + nums[i]);
             }
             return max[nums.length-1];
+        }
+    }
+
+    /**
+     * Approach   : Dynamic Programming
+     * Complexity : Time: O(n) ; Space: O(1)
+     */
+    public static class Solution3 {
+        public static int rob(int[] nums) {
+            int rob1 = 0, rob2 = 0;
+            // {rob1, rob2, n, n+1, n+2, n+3, ...}
+            for(int i = 0; i < nums.length; i++) {
+                int temp = Math.max(nums[i] + rob1, rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+            return rob2;
+        }
+    }
+
+
+    /**
+     * Approach   : Recursion(Brute Force)
+     * Complexity : Time: O(2^n) ; Space: O(n)
+     */
+    public static class Solution4 {
+        public static int max = 0;
+        public static int rob(int[] nums) {
+            max = 0;
+            Deque<Integer> indexes = new LinkedList<>();
+            robRec(nums, 0, 0, indexes);
+            return max;
+        }
+        public static void robRec(int[] nums, int index, int sum, Deque<Integer> indexes) {
+            if(index  >= nums.length) {
+                max = Math.max(max, sum);
+                return;
+            }
+
+            if(indexes.isEmpty() || (indexes.peekLast() != index - 1)) {
+                indexes.offerLast(index);
+                robRec(nums, index + 1, sum + nums[index], indexes);
+                indexes.pollLast();
+            }
+
+            robRec(nums, index + 1, sum, indexes);
         }
     }
 }
