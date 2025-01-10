02467c0955942fb0cdebb6c938d7ec21077736be
Vinod <parlapallivinod@gmail.com>
2025-01-10 17:34:29 +0530

    20250110

--- src/in/rgukt/r081247/java/Sample.java
+++ src/in/rgukt/r081247/java/Sample.java
@@ -11,103 +11,35 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 public class Sample {
-    public static class CountryPopulaion {
-        public String country;
-        public long population;
-        public CountryPopulaion(String country, long population) {
-            this.country = country;
-            this.population = population;
-        }
-        public String toString() {
-            return country + "," + population;
-        }
-        public String getCountry() {
-            return country;
-        }
-        public void setCountry(String country) {
-            this.country = country;
-        }
-        public long getPopulation() {
-            return population;
-        }
-        public void setPopulation(long population) {
-            this.population = population;
-        }
-    }
+
     public static void main(String[] args) throws Exception {
-        List<CountryPopulaion> list = new ArrayList<>();
-        String region = "Europe";
-        String keyword = "d";
-        String url = "https://jsonmock.hackerrank.com/api/countries/search?region=" + region + "&name=" + keyword;
+        String str = "aabbccdddeeeefffff";
+        Map<String, Long> counts = str.chars()
+                .filter(Character::isAlphabetic)
+                .map(Character::toLowerCase)
+                .mapToObj(Character::toString)
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting()));
 
-        String response = sendGET(url);
-        System.out.println("Response: " + response);
-        Pattern pattern = Pattern.compile("\"total_pages\":\\d+");
-        Matcher matcher = pattern.matcher(response);
-        int totalPages = 0;
-        if(matcher.find()) {
-            String totpages = matcher.group();
-            System.out.println(totpages);
-            totalPages = Integer.parseInt(totpages.split(":")[1]);
-        }
-        System.out.println("Pages: " + totalPages);
-
-        for(int i = 1; i <= totalPages; i++) {
-            String fullUrl = url + "&page=" + i;
-            response = sendGET(fullUrl);
-            System.out.println("Page: " + i + ", Response: " + response);
-            Pattern cpattern = Pattern.compile("\"name\":\"");
-            Matcher cmatcher = cpattern.matcher(response);
-            Pattern ppattern = Pattern.compile("\"population\":\\d+");
-            Matcher pmatcher = ppattern.matcher(response);
-            while (cmatcher.find() && pmatcher.find()) {
-                int start  = cmatcher.end();
-                //System.out.println(start);
-                //System.out.println(cmatcher.group());
-                int end = response.indexOf(",", start) - 1;
-                String countryName = response.substring(start, end);
-                System.out.println(countryName);
-
-                String spopulation = pmatcher.group();
-                long population = Integer.parseInt(spopulation.split(":")[1]);
-                System.out.println(population);
-                list.add(new CountryPopulaion(countryName, population));
-            }
-        }
-
-        List<CountryPopulaion> sortedList = list.stream().sorted(Comparator.comparingLong(CountryPopulaion::getPopulation).thenComparing(CountryPopulaion::getCountry))
-                .collect(Collectors.toList());
-        System.out.println(sortedList);
+        Map<Long, List<String>> c = counts.entrySet().stream()
+                .collect(
+                        Collectors.groupingBy(Map.Entry::getValue,
+                                Collectors.mapping(Map.Entry::getKey,
+                                        Collectors.toList())));
+        List<Map.Entry<Long, List<String>>> g = c.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .toList();
+        System.out.println(g);
     }
 
-    private static String sendGET(String url) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        //con.setRequestProperty("User-Agent", USER_AGENT);
-        int responseCode = con.getResponseCode();
-       // System.out.println("GET Response Code :: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            return response.toString();
-        } else {
-           return null;
-        }
-
-    }
 }
