f08264ed87cccc620b2e5136edc1d12c9bae6add
Vinod <parlapallivinod@gmail.com>
2025-02-05 08:29:19 +0530

    20250205

--- src/in/rgukt/r081247/java/algorithm/search/BinarySearchCircularArray.java
+++ src/in/rgukt/r081247/java/algorithm/search/BinarySearchCircularArray.java
@@ -3,10 +3,10 @@ package in.rgukt.r081247.java.algorithm.search;
 public class BinarySearchCircularArray {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int[] arr = {8,1,8,8,8,8,8,8,8};
+        int[] arr = {8,9,1,2,3,4,5,6,7};
         int element = 1;
         System.out.println(element);
-        int res = find(arr, element, 0, arr.length-1);
+        int res = findr(arr, element, 0, arr.length-1);
         System.out.println(res);
     }
 
@@ -57,6 +57,40 @@ public class BinarySearchCircularArray {
                     return find(arr, element, mid+1, i-1);
                 else
                     return find(arr, element, start, mid-1);
+            }
+        }
+    }
+
+    public static int findr(int[] arr, int element,  int start, int end) {
+        System.out.printf("find(%d, %d, %d)%n", element, start, end);
+        if(start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if(element == arr[mid]) {
+            return mid;
+        } else if(element < arr[mid]) {
+            if(arr[start] < arr[mid]) {
+                if(element >= arr[start])
+                    return findr(arr,element, start, mid-1);
+                else {
+                    return findr(arr, element, mid+1, end);
+                }
+            } else {
+                    return findr(arr, element, start, mid-1);
+            }
+        } else {
+            if(arr[mid] < arr[end]) {
+                if(element <= arr[end])
+                    return findr(arr,element, mid+1, end);
+                else {
+                    return findr(arr, element, start, mid-1);
+                }
+            } else {
+
+                    return findr(arr, element, mid+1, end);
+
             }
         }
     }
--- src/in/rgukt/r081247/java/interviewprep/codingdsalgooops/array/SearchinRotatedSortedArray.java
+++ src/in/rgukt/r081247/java/interviewprep/codingdsalgooops/array/SearchinRotatedSortedArray.java
@@ -73,4 +73,38 @@ public class SearchinRotatedSortedArray {
         }
     }
 
+    /**
+     * Approach: Binary Search customized
+     * Complexity : Time: O(logn) ; Space: O(1)
+     */
+    static class Solution3 {
+        public static void main(String[] args) {
+
+        }
+
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length -1;
+            while(left <= right) {
+                int mid = left + (right-left)/2;
+                if(nums[mid] == target)
+                    return mid;
+                // left sorted array
+                if(nums[left] <= nums[mid]) {
+                    if(target > nums[mid] || target < nums[left])
+                        left = mid + 1;
+                    else
+                        right = mid - 1;
+                    // right sorted array
+                } else {
+                    if(target < nums[mid] || target > nums[right])
+                        right = mid -1;
+                    else
+                        left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+
 }
